6af1448c808657990e5a8a8ccd424e6923c8acfd
rmisu <roger889@umn.edu>
2022-08-07 17:08:22 -0500

    finished adding module 14 reference code

---
+++ seeds/post-seeds.js
@@ -0,0 +1,23 @@
+const { Post } = require('../models');
+
+const postdata = [
+  {
+    title: 'Donec posuere metus vitae ipsum.',
+    post_body: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    user_id: 10
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    post_body: 'https://nasa.gov/donec.json',
+    user_id: 8
+  },
+  {
+    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    post_body: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    user_id: 1
+  }
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
